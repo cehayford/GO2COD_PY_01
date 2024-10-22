b50c71a12fae1ae1904f91d3403c02dec7347e77
cehayford <sean.alpha18@gmail.com>
2024-10-22 03:51:52 +0000

    comment #2

--- Guessing_AI.py
+++ Guessing_AI.py
@@ -3,8 +3,9 @@ from random import randint as guess_number
 def generatingRandomNumber():
     """ """
     random_number = guess_number(1, 100)
+    return random_number
 
-def guessingUserNumber(rand):
+def guessingUserNumber(rand) -> int:
     initial_guess = 0
 
     print("Guessing AI")
@@ -13,14 +14,18 @@ def guessingUserNumber(rand):
     while True:
         guess_input = int(input("You are to guess these number.\n What is the number: "))
         initial_guess += 1
-
-        if guess_input > rand:
-            print("Your guessed number{guess_input} is too high. Please, try again...")
-        if guess_input < rand:
-            print("Your guessed number{guess_input} is too low. Please, try again...")
-        else:
-            print("Well Done. You have guess the number {guess_input} correctly.")
+        # print(type(guess_input ))
+        if guess_input <=1 or guess_input >= 100:
+            print("Guess number is between 1 and 100. Guess within the range...")
             break
+        else:
+            if guess_input > rand:
+                print("Your guessed number{guess_input} is too high. Please, try again...")
+            if guess_input < rand:
+                print("Your guessed number{guess_input} is too low. Please, try again...")
+            
+            print("Well Done. You have guess the number {guess_input} correctly.")
+                
 
 if __name__ == "__main__":
     rand = generatingRandomNumber()
