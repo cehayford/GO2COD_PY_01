a384df51da08fe1bc216371e787f829eaeb80fac
cehayford <sean.alpha18@gmail.com>
2024-10-22 04:04:39 +0000

    comment #3

--- Guessing_AI.py
+++ Guessing_AI.py
@@ -12,19 +12,23 @@ def guessingUserNumber(rand) -> int:
     print("RULES: \n I have selected an integer number less than 100.\n ")
 
     while True:
-        guess_input = int(input("You are to guess these number.\n What is the number: "))
-        initial_guess += 1
-        # print(type(guess_input ))
-        if guess_input <=1 or guess_input >= 100:
-            print("Guess number is between 1 and 100. Guess within the range...")
-            break
-        else:
-            if guess_input > rand:
-                print("Your guessed number{guess_input} is too high. Please, try again...")
-            if guess_input < rand:
-                print("Your guessed number{guess_input} is too low. Please, try again...")
-            
-            print("Well Done. You have guess the number {guess_input} correctly.")
+        try:
+            guess_input = int(input("You are to guess these number.\n What is the number: "))
+            initial_guess += 1
+            # print(type(guess_input ))
+            if guess_input <=1 or guess_input >= 100:
+                print("Guess number is between 1 and 100. Guess within the range...")
+                break
+            else:
+                if guess_input > rand:
+                    print(f"Your guessed number {guess_input} is too high. Please, try again...")
+                elif guess_input < rand:
+                    print(f"Your guessed number {guess_input} is too low. Please, try again...")
+                else:
+                    print(f"Well Done. You have guess the number {guess_input} and the CPU random number {rand} correctly.")
+                    
+        except ValueError:
+            print("Invalid input! Please enter an integer between 1 and 100.")
                 
 
 if __name__ == "__main__":
